26d756efb1ad86ca54bcbe8bd1f867458decba2e
NguyenPNhan <nguyenphunhan1110@gmail.com>
2026-05-20 16:08:18 +0700

    finish task

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {
+    if (input.trim()) {
+      setTodos([...todos, 
+      {
+        id: Date.now(),
+        text: input,
+        completed: false
+      }]);
+      setInput("");
+    }
+  };
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-blue-600 to-emerald-400">
+      <div className="bg-white shadow-lg rounded-3xl p-16">
+        <h1 className="text-3xl font-bold text-center text-gray-900 mb-6">
+          REACT TO-DO LIST
+        </h1>
+        <div className="mb-4 flex">
+          <input 
+          type="text"
+          placeholder="Add a new todo"
+          className="grow px-3 py-2 border-rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          value={input}
+          onChange={(event) => setInput(event.target.value)} />
+          <button 
+          className="bg-blue-500 text-white px-4 py-2 rounded-r-lg hover:bg-blue-600 hover:cursor-pointer"
+          onClick={addTodo}>
+            Add
+          </button>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+        <ul className="space-y-2">
+          {
+            todos.map((todo) => (
+              <li
+              key={todo.id}
+              className="flex items-center p-3 rounded-lg bg-slate-100 border border-gray-200">
+                <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => setTodos(
+                  todos.map((t) => (
+                    t.id === todo.id ? {...t, completed: !t.completed} : t
+                  )) 
+                )} 
+                className="mr-2 h-5 w-5 text-blue-600" />
+                <span
+                className={`grow ${todo.completed ? "line-through text-gray-500" : "text-gray-800"}`}>
+                  {todo.text}
+                </span>
+                <button
+                onClick={() => setTodos(todos.filter((t) => t.id !== todo.id))}
+                className="ml-2 border-none p-2 rounded-lg bg-red-500 text-white hover:bg-red-600 hover:cursor-pointer">
+                  Delete
+                </button>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
+  );
 }
-
-export default App
